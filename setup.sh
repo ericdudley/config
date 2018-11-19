1a33f4a82eb918820241d515f6a5649f890cc2e1
Eric Dudley <edudley@lyft.com>
2018-11-18 16:54:58 -0800

    Added setup and aliases.wq

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+alias yinstall="sudo apt install -y "
+
+echo "System Setup Script"
+
+# Install Aliases
+cp .bash_aliases ~/.bash_aliases
+
+# Update/Upgrade
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+# Install Neovim
+yinstall neovim
+
+# Install sl
+yinstall sl
+
+# Setup ssh
+echo "Paste Private Key Content"
+
+
