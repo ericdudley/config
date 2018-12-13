ad75b6cd148f9c3eef5a23510723e110f98d25e4
Eric Dudley <edudley@lyft.com>
2018-12-12 17:53:46 -0800

    Added gitconfig.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ echo "System Setup Script"
 # Install Aliases
 cp .bash_aliases ~/.bash_aliases
 
+# Install git config
+cp .gitconfig ~/.gitconfig
+
 # Update/Upgrade
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 
